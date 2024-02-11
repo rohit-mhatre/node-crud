999b3542629ca49981c607f3cf62c30e307b4c30
Rohit Mhatre <mhatre0303rohit@gmail.com>
2024-02-11 03:45:28 -0500

    added routes

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 //import express
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import usersRoutes from './routes/users.mjs'
 const app = express();
 const PORT = 3661;
-
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
+app.get('/', (req,res) => {res.send("Hello from homepage.");})
+app.use('/users',usersRoutes);
+app.listen(PORT, () => console.log(`Server is Running on port: http://localhost:${PORT}`));
